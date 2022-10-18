d819db258d26ab7ece273545eac57be23ead6332
shafik720 <shafikrasel5@gmail.com>
2022-10-18 12:46:51 +0600

    dfgfdsg

---
+++ react-local-storage/src/Users/Users.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Users = () => {
+    return (
+        <div>
+            <h2>Hello from  users component</h2>
+        </div>
+    );
+};
+
+export default Users;
